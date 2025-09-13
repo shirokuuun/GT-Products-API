dc37e1e4609be0f40dad56bcb586972a6ab6ff70
Marcus Neo Rangel <marcusneorangel23@gmail.com>
2025-09-13 15:31:25 +0800

    env activity

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
 import postRoutes from "./src/routes/post.routes.js";
 import commentRoutes from "./src/routes/comment.routes.js";
+import config from "./src/config/index.js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json());
+
+if (config.nodeEnv === "development") {
+  app.use(morgan("dev"));
+} else {
+  null;
+}
 
 app.use("/posts", postRoutes);
 app.use("/comments", commentRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(
+    `Server is running in ${config.nodeEnv} on http://localhost:${port}`
+  );
 });
